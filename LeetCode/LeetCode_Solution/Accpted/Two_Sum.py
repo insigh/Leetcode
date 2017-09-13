082f5730277069643d60417da650806a3c5bcf5c
insighter_go <zhangchaojie@ruc.edu.cn>
2017-09-13 20:09:08 +0800

    LeetCode slolutions, Leetcode ,I am coming

---
+++ LeetCode/LeetCode_Solution/Accpted/Two_Sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict={}
+        for i,num in enumerate(nums):
+            dict[num]=i
+        for i in range (len(nums)):
+            opposite=target-nums[i]
+            if opposite in dict and dict[opposite]!=i:
+                return [i,dict[opposite]]
+        return []
+    
